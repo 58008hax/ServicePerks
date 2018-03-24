6ff258e4cd6e0e964ac5f04fd933a5c291da8161
Thomas Falsone <thomas.falsone@gmail.com>
2018-03-24 18:33:17 -0400

    start work on rewards page

---
+++ ClientApp/src/app/rewards/rewards.component.ts
@@ -0,0 +1,11 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-rewards',
+  templateUrl: './rewards.component.html',
+  styleUrls: ["./rewards.component.css"]
+})
+export class RewardsComponent {
+
+}
