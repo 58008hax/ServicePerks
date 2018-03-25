f4844577a1aa0389713b568513e44dbde0629ed9
Scott Russell <scott.tkdmaster@mac.com>
2018-03-24 20:45:20 -0400

    Scott big progress with Home Page

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -13,26 +13,31 @@ export class HomeComponent {
     this.cards = [
       {
         name: "Park Cleanup", 
-        date: "Sat., Mar. 31, 2018",
+        date: "Saturday, March 31",
         time: "3:00pm - 5:00pm",
         location: "Stevens Park, Hoboken, NJ 07030",
         points: 200
       },
       {
         name: "Homeless Shelter", 
-        date: "Sun., Apr. 1, 2018",
+        date: "Sunday, April 1",
         time: "1:00pm - 2:00pm",
         location: "The Hoboken Shelter, Hoboken, NJ 07030",
         points: 100
       },
       {
         name: "Boys and Girls Club", 
-        date: "Tues., Apr. 3, 2018",
+        date: "Tuesday, April 3",
         time: "6:00pm - 8:00pm",
         location: "Boy's & Girls Club-Hudson County, Hoboken, NJ 07030",
         points: 200
       }
     ]
+  }
+
+  goToEvent(card:any) {
+    console.log("Event Clicked:");
+    console.log(card);
   }
 
 
